b40170af00a8ff89c38fb3dd0ae128cce1df9881
srajan-iquanti <srajan@iquanti.com>
2022-06-30 17:13:00 +0530

    removed some client specific files

--- src/apps/books_catalog/routers.py
+++ src/apps/books_catalog/routers.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter(trailing_slash=False)
 
-# router.register("", views.ContentBriefView, 'content_brief')
 router.register(r"^", views.BooksCatalogueView)
 
 urlpatterns = router.urls
